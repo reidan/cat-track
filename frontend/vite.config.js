34477451c439ffd882defe736a2cb0412b2b01e4
Alyscia Smith <alysciasmith@Alyscias-Laptop.local>
2025-02-07 20:58:06 -0800

    fixed problem with vite config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,20 +2,22 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-export default defineConfig({
+export default defineConfig(({ mode }) => {
   const API_URL = process.env.VITE_API_URL || "http://localhost:8080/api"; // Fallback for safety
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-  server: {
-    allowedHosts: [
-      'ideal-brief-treefrog.ngrok-free.app',
-      'cat-track.onrender.com'
+  return {
+      plugins: [
+      react(),
+      tailwindcss(),
     ],
-    proxy: {
-      "/api": API_URL,
-      "/uploads": "http://localhost:8080",
+    server: {
+      allowedHosts: [
+        'ideal-brief-treefrog.ngrok-free.app',
+        'cat-track.onrender.com'
+      ],
+      proxy: {
+        "/api": API_URL,
+        "/uploads": "http://localhost:8080",
+      },
     },
-  },
+  };
 });
