8a668ace5296fd01ceb46d2db91d84c575a468b1
Alyscia Smith <alysciasmith@Alyscias-Laptop.local>
2025-02-12 20:00:08 -0800

    fixed an old reference to api.js

--- frontend/src/pages/BulkFoodLogs.jsx
+++ frontend/src/pages/BulkFoodLogs.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-import { Cats, Foods, BulkLogs } from "../api.js";
+import { Cats, Foods, BulkLogs } from "../api";
 const { fetchCats } = Cats;
 const { fetchFoods } = Foods;
 const { bulkUpdateLogs } = BulkLogs;
